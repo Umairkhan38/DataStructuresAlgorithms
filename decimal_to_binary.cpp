2259feb48468a1bd105f9bbaee77d2ed8d7b6471
Umair Khan <uk125896@gmail.com>
2022-12-03 19:41:48 +0530

    Commiting Initial Files

---
+++ decimal_to_binary.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<math.h>
+#include<string.h>
+using namespace std;
+
+
+int firstApproach(int n){
+
+int ans=0;
+int i=0;
+
+
+while(n!=0){
+int bit = n&1;
+    ans=(bit*pow(10,i))+ans;
+    n>>=1;
+    i++;
+
+};
+
+return ans;
+}
+
+
+//String Method
+
+string secondApproach(int n){
+    string ans;
+   
+    while(n!=0){
+        if(n&1){
+        ans+='1';
+        }else{
+            ans+='0';
+        }
+        n>>=1;
+    };
+
+    int i=0,j=ans.length()-1;
+
+    while(i<= j){
+        swap(ans[i],ans[j]);
+        i++;
+        j--;
+    }
+
+    return ans;
+}
+
+
+
+int main()
+{
+int n;
+cout<<"Enter the Decimal Number"<<endl;
+cin>>n;
+// cout<<firstApproach(n);
+cout<<secondApproach(n);
+return 0;
+}
