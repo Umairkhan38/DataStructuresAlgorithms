326a726fc6d3f06a42361b0e30533a028dc0205c
Umair Khan <uk125896@gmail.com>
2024-04-15 22:13:48 +0530

    committing after completion of queue

--- Queue/sumMaxMinofK.cpp
+++ Queue/sumMaxMinofK.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int solve(int *arr, int n, int k){
 
     //two queue to keep track of max and min elements from the windows
-        deque<int> maxi(k);
+    deque<int> maxi(k);
     deque<int> mini(k);
 
-    //Addition of first k size window
 
+    //Addition of first k size window
     for(int i=0; i<k; i++) {
 
         while(!maxi.empty() && arr[maxi.back()] <= arr[i])
@@ -24,15 +24,14 @@ int solve(int *arr, int n, int k){
     }
     int ans = 0;
 
+    //calculate ans for first window
     ans += arr[maxi.front()] + arr[mini.front()];
+
 
     //remaining windows ko process karlo
     for(int i=k; i<n; i++) {
 
-        
-
         //next window
-
         //removal
         while(!maxi.empty() && i - maxi.front() >=k) {
             maxi.pop_front();
@@ -42,13 +41,13 @@ int solve(int *arr, int n, int k){
             mini.pop_front();
         }
 
-        //addition
 
+        //addition
         while(!maxi.empty() && arr[maxi.back()] <= arr[i])
             maxi.pop_back();
 
         while(!mini.empty() && arr[mini.back()] >= arr[i])
-            mini.pop_back();
+            mini.pop_back(); 
 
         maxi.push_back(i);
         mini.push_back(i);    
@@ -64,12 +63,11 @@ int solve(int *arr, int n, int k){
 int main()
 {
 
-int arr[7] = {2, 5, -1, 7, -3, -1, -2};
-    int k = 4;
-    cout << solve(arr, 7, k) << endl;
-
+int arr[6] = {2, 3, -5, 1, 6, 7};
+    int k = 3;
+    cout << solve(arr, 6, k) << endl;
 
     return 0;
-    }
+}
 
      
